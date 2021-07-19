c8b9e4e30347852fa35a8e8c5bc81da1e31ac1db
SumiSeo <qkobr94@gmail.com>
2021-07-19 22:12:22 +0900

    created react app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../sass/main.scss";
+
+const App = () =>{
+    return (
+        <div>
+            <h2>hi</h2>
+        </div>
+    )
+};
+
+
+export default App;
